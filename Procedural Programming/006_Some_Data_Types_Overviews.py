acd6a561567e5aea9b159cc32f233bec63663eb4
omar khalid <163501945+omarkhalidhussein@users.noreply.github.com>
2025-07-09 16:29:51 +0300

    Create 006_Some_Data_Types_Overviews.py

---
+++ Procedural Programming/006_Some_Data_Types_Overviews.py
@@ -0,0 +1,22 @@
+# ----------------------------
+# type()
+# All Data in Python is Object
+# ----------------------------
+
+print(type(23))  # int => Integer
+print(type(34))  # int => Integer
+print(type(-4))  # int => Integer
+
+print(type(10.9))  # float => Floating Point Number
+print(type(1.42420))  # float => Floating Point Number
+print(type(-434.242))  # float => Floating Point Number
+
+print(type("Hello Python"))  # str => String
+
+print(type([9, 8, 7, 1, 6]))  # list => List
+
+print(type((1, 2, 3, 4, 5)))  # tuple => Tuple
+
+print(type({"One": 1, "Two": 2, "Three": 3}))  # dict => Dictionary
+
+print(type(2 == 2))  # bool => Boolean
